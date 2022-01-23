e200f0a65db192758c90c7718ab11ba555e263bd
Ase444ka <istomina.asia@yandex.ru>
2022-01-23 16:57:20 +0300

    config: change title

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     index: {
       // entry for the page
       entry: 'src/main.js',
-      title: 'Система управления пользователями',
+      title: "random beer's choicer",
     },
   },
 }
